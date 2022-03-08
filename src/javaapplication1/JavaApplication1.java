abe7b9a3b5b8e74d7755e294342b0696c31ec511
marvinp2022 <marvin.parrales@fcys.uni.edu.ni>
2022-03-08 15:36:50 -0600

    OTRO COMMIT

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -17,6 +17,11 @@ public class JavaApplication1 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("HOLA MUNDO");
+        System.out.println("HOLA MUNDO");
+        System.out.println("HOLA MUNDO");
+        System.out.println("HOLA MUNDO");
+        
+        
     }
     
 }
